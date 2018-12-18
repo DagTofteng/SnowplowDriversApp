cec58a61e090e66b23c0364a98e239a9d7c7f9b1
Dag Tofteng <Dagtofteng@gmail.com>
2018-12-18 11:40:54 +0100

    Lagt til kart

--- js/init.js
+++ js/init.js
@@ -12,17 +12,20 @@ $(document).on("click", "#login_btn", function () {
     const Http = new XMLHttpRequest();
     var url='https://www.arcgis.com/sharing/generateToken?username={0}&password={1}&referer=*&f=json';
     var un = $("#username").val();
-    var pw = $("#password").val();
+    var pw = $("#password").val();    
+
     var parsedUrl = url.replace("{0}", un).replace("{1}", pw);
 
     Http.open("GET", parsedUrl);
     Http.send();
     Http.onreadystatechange=(e)=>{
         
-        if (Http.responseText) {
+        if (Http.responseText && Http.readyState == 4 && Http.status == 200) {
             var parsedRes = JSON.parse(Http.responseText);        
             if (parsedRes && parsedRes.token) {
-                sessionStorage.setItem("broyteAppToken", parsedRes.token);            
+                sessionStorage.setItem("broyteAppToken", parsedRes.token);                  
+                initMap(parsedRes.token);        
+                
                 showStep("step2");
                 $("#userInfo").text("Hei Ståle Klommestein!")
             }
@@ -90,6 +93,8 @@ $(document).on("click", "#stopButton", function () {
 $(document).on("click", "#mapToggleButton", function () {
     $("#showMap").toggle();
     $("#hideMap").toggle();
+    $("#map").toggle();
+    $("body").height(window.innerHeight - 150);
     
 });
 
@@ -134,4 +139,100 @@ function showStep(id) {
     $("#" + id).show();
 }
 
+function initMap(token) {
+    require([              
+        "dojo/_base/array",
+        "dojo/_base/lang",    
+        "dojo/promise/all",
+        "dojo/topic",
+        "esri/Map",
+        "esri/Basemap",
+        "esri/WebMap",
+        "esri/config",    
+        "esri/geometry/Point",        
+        "esri/geometry/ScreenPoint",        
+        "esri/layers/FeatureLayer",
+        "esri/views/MapView",
+        "esri/widgets/ScaleBar",
+        "esri/request",
+        "esri/symbols/PictureMarkerSymbol",
+        "esri/Graphic",
+        "dojo/request/xhr",
+        "esri/identity/OAuthInfo",
+        "esri/identity/ServerInfo",        
+        "esri/identity/IdentityManager",
+        "dojo/domReady!"
+    ], function(
+        array,
+        lang,        
+        all,
+        topic,
+        Map,
+        Basemap,
+        WebMap,
+        esriConfig,                
+        Point,
+        ScreenPoint,
+        FeatureLayer,
+        MapView,
+        ScaleBar,
+        esriRequest,
+        PictureMarkerSymbol,
+        Graphic,
+        xhr,
+        OAuthInfo,
+        ServerInfo,
+        IdentityManager) {
+
+    IdentityManager.registerToken({
+        server: "https://testkommune.maps.arcgis.com/sharing/rest",
+        token: token
+        });
+
+    // const featureLayer = new FeatureLayer({
+    //     url: "https://services.arcgis.com/PdUYt91ohJfR04kk/arcgis/rest/services/Skedsmo_Br%C3%B8yteroder_WFL1/FeatureServer/0",
+    //     outFields: ["*"],
+    //     popupEnabled: false,
+    //     id: "RodeID"
+    //     });
+
+
+    var map = new WebMap({
+    portalItem: {
+        id: "4db252250bdb46338e49f7dbfc57c661"  
+        }
+    });
+    var referenceBasemap = "";        
+    // map.load()
+    //     // .then(function () {
+    //     //     // load the basemap to get its layers created
+    //     //     referenceBasemap = map.basemap.referenceLayers.items[0].id;
+    //     //     return map.basemap.load();
+    //     // })
+    //     .then(function () {
+    //         // grab all the layers and load them
+    //         var allLayers = map.allLayers;
+    //         var promises = allLayers.map(function (layer) {
+    //             return layer.load();
+    //         });
+    //         return all(promises.toArray());
+    //     })
+    //     .then(function (layers) {
+    //         array.forEach(layers, function (layer) {
+    //             if (layer.id == referenceBasemap) {
+    //                 layer.visible = true
+    //             }
+    //             else {
+    //                 layer.visible = false;
+    //             }
+    //         });
+    //     });
+    // Create the MapView and reference the Map in the instance
+    var view = new MapView({
+        container: "map",
+        map: map,                        
+    });
+    
+});
+}
 

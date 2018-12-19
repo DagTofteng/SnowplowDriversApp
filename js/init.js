02e28eb2f9a09257e1ba03102d09a47ac32f6648
Dag Tofteng <Dagtofteng@gmail.com>
2018-12-19 14:05:27 +0100

    added featureservice

--- js/init.js
+++ js/init.js
@@ -1,239 +1,332 @@
-var isRodeSelected = false;
-var timestampStart;
-var timestampStop;
+require([              
+    "dojo/_base/array",
+    "dojo/_base/lang",    
+    "dojo/promise/all",
+    "dojo/topic",
+    "esri/Map",
+    "esri/Basemap",
+    "esri/WebMap",
+    "esri/config",    
+    "esri/geometry/Point",        
+    "esri/geometry/ScreenPoint",        
+    "esri/layers/FeatureLayer",
+    "esri/views/MapView",
+    "esri/widgets/ScaleBar",
+    "esri/request",
+    "esri/symbols/PictureMarkerSymbol",
+    "esri/Graphic",
+    "dojo/request/xhr",
+    "esri/identity/OAuthInfo",
+    "esri/identity/ServerInfo",        
+    "esri/identity/IdentityManager",
+    "esri/tasks/QueryTask",
+    "esri/tasks/support/Query",
+    "dojo/domReady!"
+], function(
+    array,
+    lang,        
+    all,
+    topic,
+    Map,
+    Basemap,
+    WebMap,
+    esriConfig,                
+    Point,
+    ScreenPoint,
+    FeatureLayer,
+    MapView,
+    ScaleBar,
+    esriRequest,
+    PictureMarkerSymbol,
+    Graphic,
+    xhr,
+    OAuthInfo,
+    ServerInfo,
+    IdentityManager,
+    QueryTask, 
+    Query) {
 
-var selectedRode;
-
-// $("body").height(window.innerHeight);
-
-$(document).on("click", "#login_btn", function () {
-   // do login
+    var featureServiceUrl = "https://services.arcgis.com/PdUYt91ohJfR04kk/arcgis/rest/services/Skedsmo_Br%C3%B8yteroder_WFL1/FeatureServer/";
+    var timestampStart;
+    var timestampStop;
+    var selectedRodeID;
+    var selectedRodeText;
    
-    const Http = new XMLHttpRequest();
-    var ref = window.location.origin;
-    var url='https://www.arcgis.com/sharing/generateToken?username={0}&password={1}&referer={2}&f=json';
-    var un = $("#username").val();
-    var pw = $("#password").val();    
+    // if (localStorage.getItem("broyteAppTokenExpiration")) {
+    //     var tokenExpiration = localStorage.getItem("broyteAppTokenExpiration");
+    //     if (!Date.now() > tokenExpiration) {
 
-    var parsedUrl = url.replace("{0}", un).replace("{1}", pw).replace("{2}", ref);
+    //     }
+    // }
 
-    Http.open("GET", parsedUrl);
-    Http.send();
-    Http.onreadystatechange=(e)=>{
+    function initMapAndFeatureService(token) {        
+        IdentityManager.registerToken({
+            server: "https://testkommune.maps.arcgis.com/sharing/rest",
+            token: token
+        });
+
+        var map = new WebMap({
+        portalItem: {
+            id: "4db252250bdb46338e49f7dbfc57c661"  
+            }
+        });        
+        var view = new MapView({
+            container: "map",
+            map: map,                        
+        });
+        window.map = map;
+        // Get unique Rodelist
+        var query = new Query();
+        query.returnGeometry = false;
+        query.outFields = ["RodeID,RodeNavn"];
+        query.returnDistinctValues = true;
+        query.returnGeometry = false;
+        query.where = "1=1"; 
         
-        if (Http.responseText && Http.readyState == 4 && Http.status == 200) {
-            var parsedRes = JSON.parse(Http.responseText);        
-            if (parsedRes && parsedRes.token) {
-                sessionStorage.setItem("broyteAppToken", parsedRes.token);                  
-                initMap(parsedRes.token);        
-                
-                showStep("step2");
-                $("#userInfo").text("Hei Ståle Klommestein!")
+        var rodeArray = [];
+        
+        var queryTaskLine = new QueryTask({
+            url: featureServiceUrl + "0?token=" + token
+        });
+
+        // Kjør 2 queryTasker (mot linje og polygon)
+        var numberOfQueryTasksCompleted = 0;
+        queryTaskLine.execute(query).then(function(results){            
+            array.forEach(results.features, function (item) {
+                if (item.attributes.RodeID && item.attributes.RodeNavn) {
+                    var existInArray = false;
+                    for(i = 0; i < rodeArray.length; i++) {
+                        if (rodeArray[i].RodeID == item.attributes.RodeID && rodeArray[i].RodeNavn == item.attributes.RodeNavn) {
+                            existInArray = true;
+                            break;
+                        }
+                    }
+                    if (!existInArray) {
+                        rodeArray.push({ RodeID: item.attributes.RodeID, RodeNavn: item.attributes.RodeNavn });
+                    }
+                }
+            });
+    
+            rodeArray.sort();  
+            numberOfQueryTasksCompleted++;
+            var queryTaskPolygon = new QueryTask({
+                url: featureServiceUrl + "1?token=" + token
+            });
+            queryTaskPolygon.execute(query).then(function(results){            
+                array.forEach(results.features, function (item) {
+                    if (item.attributes.RodeID && item.attributes.RodeNavn) {
+                        var existInArray = false;
+                        for(i = 0; i < rodeArray.length; i++) {
+                            if (rodeArray[i].RodeID == item.attributes.RodeID && rodeArray[i].RodeNavn == item.attributes.RodeNavn) {
+                                existInArray = true;
+                                break;
+                            }
+                        }
+                        if (!existInArray) {
+                            rodeArray.push({ RodeID: item.attributes.RodeID, RodeNavn: item.attributes.RodeNavn });
+                        }
+                    }
+                });    
+                rodeArray.sort();  
+                numberOfQueryTasksCompleted++;          
+                if (numberOfQueryTasksCompleted == 2) {
+                    buildRodeDropdown(rodeArray);
+                }
+            }); 
+
+           
+        }); 
+        
+    }
+
+    function buildRodeDropdown(data) {
+        var rodeDropdownHtml = "<option selected>Velg rode...</option>{0}";
+        array.forEach(data, function (rode) {
+            var optionHtml = "<option value='" + rode.RodeID + "'>" + rode.RodeNavn + "</option>";
+            rodeDropdownHtml += optionHtml;
+        });
+
+        $("#rodeDropdown").html(rodeDropdownHtml);
+
+        if (localStorage.getItem("broyteAppLastRodeID")) {
+            var lastRodeID = localStorage.getItem("broyteAppLastRodeID");
+            $("#rodeDropdown").val(lastRodeID).trigger("change");
+            // var newValueText = $("#rodeDropdown").find("option:selected").text();
+            // var newValueID = lastRodeID          
+            // selectedRodeText = newValueText;
+            // selectedRodeID = newValueID;
+            // showStep("step3");
+            // $("#choosenRode").html(newValueText);
+            // $("#timeInfoText").html("");            
+            }
+        else {        
+                selectedRodeText = null;
+                selectedRodeID = null;
+            }
+    }
+    
+
+    $(document).on("click", "#login_btn", function () {
+    // do login
+    
+        const Http = new XMLHttpRequest();
+        var ref = window.location.origin;
+        // Expiration 1440 = 1 døgn
+        var url='https://www.arcgis.com/sharing/generateToken?username={0}&password={1}&referer={2}&expiration=1440&f=json';
+        var un = $("#username").val();
+        var pw = $("#password").val();    
+
+        var parsedUrl = url.replace("{0}", un).replace("{1}", pw).replace("{2}", ref);
+
+        Http.open("GET", parsedUrl);
+        Http.send();
+        Http.onreadystatechange=(e)=>{
+            
+            if (Http.responseText && Http.readyState == 4 && Http.status == 200) {
+                var parsedRes = JSON.parse(Http.responseText);        
+                if (parsedRes && parsedRes.token) {
+                    sessionStorage.setItem("broyteAppToken", parsedRes.token);   
+                    var expiration = new Date();
+                    sessionStorage.setItem("broyteAppTokenExpiration", expiration);   
+
+                    initMapAndFeatureService(parsedRes.token);        
+                    
+                    showStep("step2");
+                    $("#userInfo").text("Hei Ståle Klommestein!")
+                }
+                else {
+                    $("#loginText").text("Brukernavn og/eller passord stemmer ikke.")
+                }
             }
             else {
                 $("#loginText").text("Brukernavn og/eller passord stemmer ikke.")
             }
         }
-        else {
-            $("#loginText").text("Brukernavn og/eller passord stemmer ikke.")
-        }
-    }
-   
-});
+    
+    });
 
-$(document).on("change", "#rodeDropdown", function (e) {
-    var newVal = e.currentTarget.value;
-    if (newVal) {
-        isRodeSelected = true;
-        selectedRode = newVal;
+    $(document).on("change", "#rodeDropdown", function (e) {
+        var newValueText = $(this).find("option:selected").text();
+        var newValueID = e.currentTarget.value;
+        if (newValueText) {        
+            selectedRodeText = newValueText;
+            selectedRodeID = newValueID;
+            showStep("step3");
+            $("#timeInfoText").html("");
+            localStorage.setItem("broyteAppLastRodeID", newValueID);        
+        }
+        else {        
+            selectedRodeText = null;
+            selectedRodeID = null;
+        }
+        $("#choosenRode").html(newValueText);
         showStep("step3");
-        $("#timeInfoText").html("");
-    }
-    else {
-        isRodeSelected = false;
-        selectedRode = null;
-    }
-    $("#choosenRode").html(newVal);
-    showStep("step3");
-});
-
-$(document).on("click", "#changeRode", function (e) {    
-    timestampStop = Date.now();    
-    var totalTime = timestampStop - timestampStart;
-    $("#stopButton").click();
-    showStep("step2");
-});
-
-$(document).on("click", "#startButton", function () {
-    $("#startButton").hide();
-    $("#stopButton").show();        
-    
-    timestampStart = Date.now();
-    var timestampStartDateReadable = new Date(timestampStart).toLocaleDateString();
-    var timestampStartTimeReadable = new Date(timestampStart).toLocaleTimeString();
-    var startText = selectedRode + " ble startet brøytet<br/>" + timestampStartDateReadable + " " + timestampStartTimeReadable;
-    $("#timeInfoText").html(startText);
-    $("#counter").text("00:00:00");
-    incrementSeconds();
-    // Send timestamp kall api
-});
-$(document).on("click", "#stopButton", function () {
-    $("#startButton").show();
-    $("#stopButton").hide();
-    
-    timestampStop = Date.now();        
-    var timestampStopDateReadable = new Date(timestampStop).toLocaleDateString();
-    var timestampStopTimeReadable = new Date(timestampStop).toLocaleTimeString();
-    
-    var totalTimeReadable = diff_hours(timestampStart, timestampStop);    
-    var stopText = selectedRode + " ble avsluttet<br/>" + timestampStopDateReadable + " " + timestampStopTimeReadable + "<br><br>Tid brukt: " + totalTimeReadable;
-    $("#timeInfoText").html(stopText);    
-    
-});
-
-$(document).on("click", "#mapToggleButton", function () {
-    $("#showMap").toggle();
-    $("#hideMap").toggle();
-    $("#map").toggle();
-    $("body").height(window.innerHeight - 150);
-    
-});
-
-
-function diff_hours(startTime, stopTime) 
- {
-    var diffInMs = stopTime - startTime;    
-
-    var seconds = diffInMs / 1000;
-    var hours = parseInt( seconds / 3600 ); 
-    seconds = seconds % 3600; 
-    var minutes = parseInt( seconds / 60 ); 
-    seconds = Math.floor(seconds % 60);
-
-    // var hours   = Math.floor(diffInMs / 3600);
-    // var minutes = Math.floor(diffInMs / 60000)  
-    // var seconds = ((diffInMs % 60000) / 1000).toFixed(0);  
-
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
-    var outputTime = hours+':'+minutes+':'+seconds;
-    return outputTime;
-    //(new Date(stopTime).getTime() - new Date(startTime).getTime()) / 1000;  
- }
-
- function incrementSeconds() {
-    var seconds = 0;
-    
-    setInterval(function () {
-        var counter = diff_hours(timestampStart, Date.now())
-        $("#counter").text(counter);
-        seconds++;
-    }, 1000);   
-    
-}
-
-function showStep(id) {
-    $("#step1").hide();
-    $("#step2").hide();
-    $("#step3").hide();
-    $("#" + id).show();
-}
-
-function initMap(token) {
-    require([              
-        "dojo/_base/array",
-        "dojo/_base/lang",    
-        "dojo/promise/all",
-        "dojo/topic",
-        "esri/Map",
-        "esri/Basemap",
-        "esri/WebMap",
-        "esri/config",    
-        "esri/geometry/Point",        
-        "esri/geometry/ScreenPoint",        
-        "esri/layers/FeatureLayer",
-        "esri/views/MapView",
-        "esri/widgets/ScaleBar",
-        "esri/request",
-        "esri/symbols/PictureMarkerSymbol",
-        "esri/Graphic",
-        "dojo/request/xhr",
-        "esri/identity/OAuthInfo",
-        "esri/identity/ServerInfo",        
-        "esri/identity/IdentityManager",
-        "dojo/domReady!"
-    ], function(
-        array,
-        lang,        
-        all,
-        topic,
-        Map,
-        Basemap,
-        WebMap,
-        esriConfig,                
-        Point,
-        ScreenPoint,
-        FeatureLayer,
-        MapView,
-        ScaleBar,
-        esriRequest,
-        PictureMarkerSymbol,
-        Graphic,
-        xhr,
-        OAuthInfo,
-        ServerInfo,
-        IdentityManager) {
-
-    IdentityManager.registerToken({
-        server: "https://testkommune.maps.arcgis.com/sharing/rest",
-        token: token
-        });
-
-    // const featureLayer = new FeatureLayer({
-    //     url: "https://services.arcgis.com/PdUYt91ohJfR04kk/arcgis/rest/services/Skedsmo_Br%C3%B8yteroder_WFL1/FeatureServer/0",
-    //     outFields: ["*"],
-    //     popupEnabled: false,
-    //     id: "RodeID"
-    //     });
-
-
-    var map = new WebMap({
-    portalItem: {
-        id: "4db252250bdb46338e49f7dbfc57c661"  
-        }
     });
-    var referenceBasemap = "";        
-    // map.load()
-    //     // .then(function () {
-    //     //     // load the basemap to get its layers created
-    //     //     referenceBasemap = map.basemap.referenceLayers.items[0].id;
-    //     //     return map.basemap.load();
-    //     // })
-    //     .then(function () {
-    //         // grab all the layers and load them
-    //         var allLayers = map.allLayers;
-    //         var promises = allLayers.map(function (layer) {
-    //             return layer.load();
-    //         });
-    //         return all(promises.toArray());
-    //     })
-    //     .then(function (layers) {
-    //         array.forEach(layers, function (layer) {
-    //             if (layer.id == referenceBasemap) {
-    //                 layer.visible = true
-    //             }
-    //             else {
-    //                 layer.visible = false;
-    //             }
-    //         });
-    //     });
-    // Create the MapView and reference the Map in the instance
-    var view = new MapView({
-        container: "map",
-        map: map,                        
-    });
-    
-});
-}
 
+    $(document).on("click", "#changeRode", function (e) {    
+        timestampStop = Date.now();    
+        var totalTime = timestampStop - timestampStart;
+        $("#stopButton").click();
+        showStep("step2");
+    });
+
+    $(document).on("click", "#startButton", function () {
+        var feature = getFeatureForID(selectedRodeID);
+        $("#startButton").hide();
+        $("#stopButton").show();        
+        
+        timestampStart = Date.now();
+        var timestampStartDateReadable = new Date(timestampStart).toLocaleDateString();
+        var timestampStartTimeReadable = new Date(timestampStart).toLocaleTimeString();
+        var startText = selectedRodeText + " ble startet brøytet<br/>" + timestampStartDateReadable + " " + timestampStartTimeReadable;
+        $("#timeInfoText").html(startText);
+        $("#counter").text("00:00:00");
+        incrementSeconds();
+        // Send timestamp kall api
+    });
+    $(document).on("click", "#stopButton", function () {
+        $("#startButton").show();
+        $("#stopButton").hide();
+        
+        timestampStop = Date.now();        
+        var timestampStopDateReadable = new Date(timestampStop).toLocaleDateString();
+        var timestampStopTimeReadable = new Date(timestampStop).toLocaleTimeString();
+        
+        var totalTimeReadable = diff_hours(timestampStart, timestampStop);    
+        var stopText = selectedRodeText + " ble avsluttet<br/>" + timestampStopDateReadable + " " + timestampStopTimeReadable + "<br><br>Tid brukt: " + totalTimeReadable;
+        $("#timeInfoText").html(stopText);    
+        
+    });
+
+    $(document).on("click", "#mapToggleButton", function () {
+        $("#showMap").toggle();
+        $("#hideMap").toggle();
+        $("#map").toggle();
+        $("body").height(window.innerHeight - 150);
+        
+    });
+
+    function diff_hours(startTime, stopTime) 
+    {
+        var diffInMs = stopTime - startTime;    
+
+        var seconds = diffInMs / 1000;
+        var hours = parseInt( seconds / 3600 ); 
+        seconds = seconds % 3600; 
+        var minutes = parseInt( seconds / 60 ); 
+        seconds = Math.floor(seconds % 60);
+
+        // var hours   = Math.floor(diffInMs / 3600);
+        // var minutes = Math.floor(diffInMs / 60000)  
+        // var seconds = ((diffInMs % 60000) / 1000).toFixed(0);  
+
+        if (hours   < 10) {hours   = "0"+hours;}
+        if (minutes < 10) {minutes = "0"+minutes;}
+        if (seconds < 10) {seconds = "0"+seconds;}
+        var outputTime = hours+':'+minutes+':'+seconds;
+        return outputTime;
+        //(new Date(stopTime).getTime() - new Date(startTime).getTime()) / 1000;  
+    }
+
+    function incrementSeconds() {
+        var seconds = 0;
+        
+        setInterval(function () {
+            var counter = diff_hours(timestampStart, Date.now())
+            $("#counter").text(counter);
+            seconds++;
+        }, 1000);   
+        
+    }
+
+    function showStep(id) {
+        $("#step1").hide();
+        $("#step2").hide();
+        $("#step3").hide();
+        $("#" + id).show();
+    }
+
+    function getFeatureForID(id) {
+        var layer = window.map.allLayers.find(function(layer) {
+            return layer.layerId === 0;
+           });
+           const attributes = {};
+           attributes["OBJECTID"] = 2;
+           attributes["EmpTidsforbrukMinutter"] = 41;           
+         
+           const updateFeature =  new Graphic({  
+            geometry: null,                        
+            attributes: attributes
+           });
+           const edits = {
+            updateFeatures: [updateFeature]
+            }
+         
+         
+           const promise = layer.applyEdits(edits).then(function(result){             
+             var test = result;
+           });        
+
+    }
+        
+});
